aa0f82561ebb1b66d9210662d071da04658cdb4f
KEILLIM <keila90.if@gmail.com>
2022-01-11 22:06:19 -0300

    commit inicial

---
+++ src/main/java/com/example/mytaskbackend/MyTaskBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.mytaskbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyTaskBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyTaskBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mytaskbackend/MyTaskBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mytaskbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyTaskBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
